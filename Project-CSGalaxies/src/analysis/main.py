de792de37a991c077080e0fcf71f98139b2b4148
Gregory Peruzzo Fiel <gregexbr@hotmail.com>
2024-10-16 16:33:02 -0300

    fix marvin api

--- Project-CSGalaxies/src/analysis/main.py
+++ Project-CSGalaxies/src/analysis/main.py
@@ -2,7 +2,7 @@ from api.MarvinClient import Marvin
 
 def main():
     marvin = Marvin()
-    marvin_map = marvin.get_api('12772-12705')
+    marvin_map = marvin.get_api('name=12772-12705')
     print(marvin_map)
 
 if __name__ == '__main__':
