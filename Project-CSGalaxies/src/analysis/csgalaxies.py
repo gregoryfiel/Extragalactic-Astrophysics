b616e4af1245ff34c6692925ceeed1a00901de5a
Gregory Peruzzo Fiel <gregexbr@hotmail.com>
2024-10-17 21:39:42 -0300

    csgalaxies update with pyspark

--- Project-CSGalaxies/src/analysis/csgalaxies.py
+++ Project-CSGalaxies/src/analysis/csgalaxies.py
@@ -1,26 +1,44 @@
+import os
+import sys
+import shutil
+
 from api.MarvinClient import Marvin
-import pyspark
 from pyspark.sql import SparkSession
-import json
+from pyspark.sql.functions import col, explode
 
-# Inicializa a SparkSession
-spark = SparkSession.builder.master("local[*]").getOrCreate()
+# Configurações do PySpark
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
-# Cria uma instância da classe Marvin
+# Iniciando SparkSession
+spark = SparkSession.builder \
+    .master("local") \
+    .appName("PySpark Installation Test") \
+    .config("spark.hadoop.fs.permissions", "false") \
+    .getOrCreate()
+
+# Obtendo dados do Marvin API
 marvin = Marvin()
+map_name = '8485-1901'
+marvin_data = marvin.get_api(map_name, 'SPX', 'MILESHC-MASTARSSP')
 
-# Obtém os dados da API
-marvin_map = marvin.get_api('8485-1901', 'SPX', 'MILESHC-MASTARSSP')
+# Criando DataFrame e explodindo a coluna "shape"
+data_DF = spark.createDataFrame([marvin_data])
+data_DF = data_DF.withColumn("shape", explode(col("shape")))
 
-# Agora marvin_map deve conter os dados retornados pela API
-# Se marvin_map for um dicionário ou lista, você pode criar um DataFrame assim:
-if isinstance(marvin_map, dict):
-    # Converte o dicionário para uma lista com um único item
-    df = spark.read.json(spark.sparkContext.parallelize([json.dumps(marvin_map)]))
-elif isinstance(marvin_map, list):
-    # Se for uma lista, converta-a diretamente para DataFrame
-    df = spark.read.json(spark.sparkContext.parallelize(marvin_map))
+# Mostrando os dados
+data_DF.show()
 
-# Exibe o esquema e os dados
-df.printSchema()
-df.show()
+# Salvando em um diretório temporário como uma única partição
+temp_dir = f"../data/landing/temp_{map_name}"
+data_DF.coalesce(1).write.csv(temp_dir, sep=';', mode='overwrite', header=True)
+
+# Encontrando o arquivo CSV gerado e renomeando
+for file_name in os.listdir(temp_dir):
+    if file_name.endswith(".csv"):
+        # Movendo o arquivo CSV para o destino final com o nome correto
+        shutil.move(os.path.join(temp_dir, file_name), f"../data/landing/{map_name}.csv")
+        break
+
+# Remover o diretório temporário
+shutil.rmtree(temp_dir)
