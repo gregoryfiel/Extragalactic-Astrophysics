3163a26eecff264efeb07fb26b70394273143cb5
Gregory Peruzzo Fiel <gregexbr@hotmail.com>
2024-11-05 14:28:38 -0300

    reworking the fit_gauss script

--- fit_gaussian_spectrum/Script_Gregory/main.py
+++ fit_gaussian_spectrum/Script_Gregory/main.py
@@ -2,3 +2,6 @@ def main():
     """Entrada de um ou mais arquivos cxt.
     """
     pass
+
+if __name__ == '__main__':
+    main()
---
+++ fit_gaussian_spectrum/Script_Gregory/models/model_plotting.py
@@ -0,0 +1,11 @@
+from  matplotlib import pyplot
+
+class PlottingGaussian:
+    def __init__(self):
+        pass
+    
+    def plot(self):
+        pass
+    
+    def export_plots(self):
+        pass
